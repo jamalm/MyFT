5917970888f6d6474812b109dfd4754367e1fcc0
Jamal Mahmoud <bluedocjamal@hotmail.co.uk>
2017-04-24 12:16:05 +0100

    fixed Authentication

--- src/Auth.c
+++ src/Auth.c
@@ -15,9 +15,11 @@ int Authenticate(char *user, char *password)
 	char* storedPass;
 	//read file for stored usernames
 	char** auths = getusers();
+	
+	printf("User1: %s\nUser2: %s\nUSer3: %s\n", auths[0], auths[1], auths[2]);
 	for(i=0;i<MAXUSERS;i++)
 	{
-		if(auths[i] != NULL)
+		if(auths[i] != "")
 		{
 			printf("\n---------------------------\nAuth combi string: %s\n", auths[i]);
 			//split by username and password
@@ -25,36 +27,33 @@ int Authenticate(char *user, char *password)
 			strncpy(storedUser, auths[i], sizeof(storedUser));
 			storedPass[strcspn(storedPass, "\n")] = 0;
 			
-			printf("Auth Password: %s\nAuth Username: %s\n-----------------------------\n", storedPass, auths[i]);
+			printf("Auth Password: %s\nAuth Username: %s\n-----------------------------\n", storedPass, storedUser);
 
 			if((strcmp(user, storedUser) == 0) && (strcmp(password, storedPass) == 0))
 			{
 				printf("Comparison return: TRUE\n");
-				for(j=0;i<MAXUSERS;i++)
-					free(auths[j]);
-				free(auths);
 				return 1;
 			}
 		}
 	}
 	printf("Comparison return: FALSE\n");
-	
-	for(i=0;i<MAXUSERS;i++)
-		free(auths[i]);
-	free(auths);
 	return 0;
 }
 
 char ** getusers()
 {
+	printf("LOADING USERS FILE...\n");
 	int i;
 	int counter = 0;
 	char buffer[MAXUNAMESIZE];
+	
+	
 	//allocate a size for the number of users allowed to log into the system
 	char** auths = malloc(MAXUSERS * sizeof(char *));
+	
 	for(i=0; i<MAXUSERS; i++)
 	{
-		auths[i] = malloc(MAXUNAMESIZE);
+		auths[i] = malloc(MAXUNAMESIZE+1);
 	}
 	
 	FILE* fp = fopen(USERS, "r");
@@ -64,10 +63,10 @@ char ** getusers()
 	}
 	while(fgets(buffer, sizeof(buffer), fp) != NULL)
 	{
-		auths[counter] = buffer;
+		strncpy(auths[counter], buffer, sizeof(buffer));
+		fprintf(stdout, "Added: %s from buffer with string %s\nLOCATION: %d", auths[counter], buffer, counter);
 		counter++;
 	}
-	
 	fclose(fp);
 	return auths;
 }
